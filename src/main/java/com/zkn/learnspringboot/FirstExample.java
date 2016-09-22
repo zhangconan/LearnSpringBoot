4c5a6e8a5ecb58f76987cedaac1e95fe86922933
zknxx <zhangconancomputer@126.com>
2016-09-23 00:04:30 +0800

    新增P6spy跟踪sql

--- src/main/java/com/zkn/learnspringboot/FirstExample.java
+++ src/main/java/com/zkn/learnspringboot/FirstExample.java
@@ -3,6 +3,7 @@ package com.zkn.learnspringboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @EnableAutoConfiguration
+@ImportResource(locations = {"classpath:dubbo-provider.xml"})
 @ComponentScan
 public class FirstExample {
 
--- src/main/java/com/zkn/learnspringboot/service/impl/PersonServiceImpl.java
+++ src/main/java/com/zkn/learnspringboot/service/impl/PersonServiceImpl.java
@@ -1,15 +1,17 @@
 package com.zkn.learnspringboot.service.impl;
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.zkn.learnspringboot.dao.PersonDAO;
 import com.zkn.learnspringboot.domain.PersonDomain;
 import com.zkn.learnspringboot.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 /**
  * Created by zkn on 2016/8/11.
  */
-@Service("personService")
+//@Service("personService")
+//Service一定要用dubbo的Service
+@Service
 public class PersonServiceImpl implements PersonService {
 
     @Autowired
