50d46c3088d73e6c2aa2a3cb3f2a0be4ab2ae78c
zknxx <zhangconancomputer@126.com>
2016-11-20 20:56:43 +0800

    新增单元测试

---
+++ src/test/java/com/zkn/learnspringboot/service/test/BaseTestService.java
@@ -0,0 +1,15 @@
+package com.zkn.learnspringboot.service.test;
+
+import com.zkn.learnspringboot.FirstExample;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by zkn on 2016/11/20.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = FirstExample.class)
+public class BaseTestService {
+
+}
--- src/test/java/com/zkn/learnspringboot/service/test/TestService.java
+++ src/test/java/com/zkn/learnspringboot/service/test/TestService.java
@@ -1,20 +1,14 @@
 package com.zkn.learnspringboot.service.test;
 
-import com.zkn.learnspringboot.FirstExample;
 import com.zkn.learnspringboot.service.PersonService;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
  * Created by wb-zhangkenan on 2016/11/18.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = FirstExample.class)
-public class TestService {
+
+public class TestService extends BaseTestService{
     @Autowired
     private PersonService personService;
     @Test
