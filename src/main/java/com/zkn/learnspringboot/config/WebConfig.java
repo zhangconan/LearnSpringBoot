b28371cd63d8433c38b1966982e0f5589b21c8f2
zknxx <zhangconancomputer@126.com>
2016-11-30 23:47:51 +0800

    新增加载静态资源

--- src/main/java/com/zkn/learnspringboot/config/WebConfig.java
+++ src/main/java/com/zkn/learnspringboot/config/WebConfig.java
@@ -16,7 +16,8 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        //registry.addResourceHandler("/templates/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX+"/templates/",ResourceUtils.CLASSPATH_URL_PREFIX+"/image/");
+        registry.addResourceHandler("/templates/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX+"/templates/");
+        registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX+"/static/");
         super.addResourceHandlers(registry);
     }
     */
